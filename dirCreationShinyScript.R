c48217da3958f27d9d2c2015331cc1a6e32ba13f
bott-CorsairV2 <benjaminott@trentu.ca>
2020-12-22 19:13:23 -0500

    added the working dir creation script back from server

--- dirCreationShinyScript.R
+++ dirCreationShinyScript.R
@@ -2,13 +2,14 @@
 library("tiff")
 library("png")
 library("data.table")
+library("stringr")
 
 
 #finds the year based off of the name
 .get_image_year <- function(imageName){
   splitName <- strsplit(imageName, "-")
   date <- splitName[[1]][length(splitName[[1]]) - 1]
-  yearVector <- strsplit(date,"")[[1]][1:4]
+  yearVector <- strsplit(date,"")[[1]][1:min(4, length(strsplit(date,"")[[1]]))]
   year <- paste0(yearVector[1], yearVector[2], yearVector[3], yearVector[4])
   return(year)
 }
@@ -48,15 +49,15 @@ imageChecking <- function(imageName,locationToCheck){
 
 
 
-# toDir <- "~/Magneto/creationShinyDir//"
-#
-#
-#
-# # First part organizes the pngs from the conversion from .tiff on the server----
-#
-# fromDir <- "~/Magneto/fromDir/"
-#
-#
+toDir <- "~/Magneto/creationShinyDir//"
+
+
+
+# First part organizes the pngs from the conversion from .tiff on the server----
+
+fromDir <- "~/Magneto/fromDir/"
+
+
 # browser()
 # for (i in list.files(fromDir)){#lists files in that dir, no subdirectorys please
 #   found <- FALSE
@@ -68,8 +69,8 @@ imageChecking <- function(imageName,locationToCheck){
 #       file.copy(from = paste0(fromDir, i), to = j)
 #       found <- TRUE
 #       print("found")
+#       break
 #     }
-#    break
 #   }
 #   if (isFALSE(found)) { # put into main dir for people to process in the future
 #     file.copy(from= paste0(fromDir, i), to = pwd)
@@ -85,37 +86,75 @@ fromDirTiffOrigionals <-"~/Magneto/fromDirTiffs/"
 
 #browser()
 badOnes <- vector()
-for (i in list.files(fromDirTiffOrigionals, recursive = TRUE)){
+for (i in list.files(fromDirTiffOrigionals, recursive = TRUE)){ #recursive list of files
   found <- FALSE
   splitName <- strsplit(i, "/")
   imageName <- last(splitName[[1]])
+  print(imageName)
   pwd <- dir_str(imageName = i, pathToWorkingDir = toDirtiff)
 
-  for (j in list.dirs(pwd)) {
-    if (length(list.files(path = j, pattern = imageName)) >=1){
+  for (j in list.dirs(pwd)) { #usually just the blank year and the failed to process inside
+    if (length(list.files(path = j, pattern = imageName)) >= 1){
       if (length(list.files(path = j, pattern = paste0(imageName, ".png"))) != 0){
         file.copy(from = paste0(fromDirTiffOrigionals, i), to = j)
         found <- TRUE
-      }
-    }
-    else{ # need to check the failed to process before creating the .png....
-      firstPartOfName <- imageChecking(imageName, 1)
-      if (as.numeric(file.info(paste0(fromDirTiffOrigionals,i))[1]) != 0) {
-        if (firstPartOfName == "AGC" || firstPartOfName == "TOR") {
-          if(isTRUE(isTiff(i))){
-            imageImport <- readTIFF(paste0(fromDirTiffOrigionals, i))
-            writePNG(imageImport, target = paste0(pwd, imageName,".png"))
-            file.copy(from = paste0(fromDirTiffOrigionals, i), to = j)
-            found <- TRUE
-          }
-        }
       }
     }
     if (isTRUE(found)) {
       break
     }
   }
-  if (isFALSE(found)){
+  if (isFALSE(found)){ #if no png is found for that image, it will create it
+    for (j in list.dirs(pwd)) {
+      if (length(list.files(path = j, pattern = imageName)) >=1){ #instead just look for the image name
+        firstPartOfName <- imageChecking(imageName, 1)
+        if (as.numeric(file.info(paste0(fromDirTiffOrigionals,i))[1]) != 0) {
+          if (firstPartOfName == "AGC" || firstPartOfName == "TOR") {
+            if(isTRUE(isTiff(i))){
+              imageImport <- tryCatch(readTIFF(paste0(fromDirTiffOrigionals, i)), error = function(e) e) #catches corrupt files
+
+              if (inherits(imageImport, "error")) {
+                badOnes <- c(badOnes, paste0(i, "corrupt"))
+                found <- TRUE
+                break
+              }
+
+              writePNG(imageImport, target = paste0(j, "/", imageName,".png"))
+              file.copy(from = paste0(fromDirTiffOrigionals, i), to = j)
+              found <- TRUE
+
+            }
+          }
+        }
+      }
+      if (isTRUE(found)) {
+        break
+      }
+    }
+    if (isFALSE(found)) {
+      # not found anywhere, then no .png and no digitization yet
+      firstPartOfName <- imageChecking(imageName, 1)
+      if (as.numeric(file.info(paste0(fromDirTiffOrigionals,i))[1]) != 0) {
+        if (firstPartOfName == "AGC" || firstPartOfName == "TOR") {
+          if (isTRUE(isTiff(i))) {
+            imageImport <- tryCatch(readTIFF(paste0(fromDirTiffOrigionals, i)),error = function(e) e) #catches corrupt files
+
+            if (inherits(imageImport, "error")) {
+              badOnes <- c(badOnes, paste0(i, "corrupt"))
+              found <- TRUE
+              next
+            }
+
+            writePNG(imageImport, target = paste0(pwd, imageName,".png"))
+            file.copy(from = paste0(fromDirTiffOrigionals, i), to = pwd)
+            found <- TRUE
+
+          }
+        }
+      }
+    }
+  }
+  if (isFALSE(found)) { # if still not found, then bad image name most likely
     badOnes <- c(badOnes, i)
   }
 }
